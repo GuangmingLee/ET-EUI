d96e4bb615258bc473246a67174027a1ffcd0a08
GuangmingLee <1040660846@qq.com>
2023-06-05 23:52:58 +0800

    创建角色相关逻辑

---
+++ Unity/Codes/Model/Demo/Role/RoleInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace ET
+{
+    public enum RoleInfoState
+    {
+        Normal =0,
+        Freeze,
+    }
+
+    public class RoleInfo: Entity, IAwake
+    {
+        public string Name;
+        public int State;
+        public long AccountId;
+        public long CreateTime;
+        public int ServerId;
+        public long LastLoginTime;
+    }
+}
